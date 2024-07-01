a19407a73744d4c4e2a1eecc47ee41fd28ca6064
Pratheeksh7 <pratheeksh.729@gmail.com>
2024-07-01 09:48:16 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,48 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-void bfs(int A[10][10],int n,int visited[10],int source)
-{
-    int q[10],rear=-1,front=0,i,delNode;
-    visited[source]=1;
-    q[++rear]=source;
-    while(front<=rear)
-    {
-        delNode=q[front++];
-        for(i=1;i<=n;i++)
+#include<string.h>
+int count;
+int bfsm(char t[20],char p[10]){
+    int m,n,i,j;
+    n=strlen(t);
+    m=strlen(p);
+    for(i=0;i<=n-m;i++){
+        j=0;
+        while(j<m && p[j]==t[i+j])
         {
-            if(A[delNode][i]==1 && visited[i]==0)
-            {
-                q[++rear]=i;
-                visited[i]=1;
-            }
+            j=j+1;
+            count++;
         }
+        count++;
+        if (j==m)
+            return i+1;
     }
+    return -1;
+
 }
 int main()
 {
-   int n,A[10][10],i,j,visited[10],count=0;
-   printf("\nEnter the number of nodes:");
-   scanf("%d",&n);
-   printf("\nRead adjacency matrix\n");
-   for(i=1;i<=n;i++)
-   {
-       for(j=1;j<=n;j++)
-           scanf("%d",&A[i][j]);
-
-   }
-   for(i=1;i<=n;i++)
-    visited[i]=0;
-   for(i=1;i<=n;i++)
-   {
-       if(visited[i]==0)
-       {
-             bfs(A,n,visited,i);
-             count++;
-       }
-   }
-   if(count==1)
-    printf("\nGraph is connected,1 component");
-   else
-    printf("\nGraph is not connected , %d component",count);
+    char t[20],p[10];
+    int res;
+    printf("Enter the text\t");
+    scanf("%s",t);
+    printf("Enter the pattern\t");
+    scanf("%s",p);
+    res=bfsm(t,p);
+    if(res==-1)
+        printf("Pattern not found");
+    else
+        printf("Pattern found at %d position",res);
+    printf("\n Number of comparisons=%d",count-1);
     return 0;
 }
